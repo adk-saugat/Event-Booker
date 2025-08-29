5731e56376da679736602b3d8a473a339247bc1f
adk-saugat <adhikarisaugat34@gmail.com>
2025-08-28 23:51:59 -0500

    added save and create events to db

--- models/event.go
+++ models/event.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/event-booker/db"
+)
 
 type Event struct{
-	ID int
+	ID int64
 	Name string `binding:"required"`
 	Description string `binding:"required"`
 	Location string `binding:"required"`
@@ -13,10 +17,49 @@ type Event struct{
 
 var events = []Event{}
 
-func (event Event) Save(){
+func (event Event) Save() error{
+	query := `
+		INSERT INTO events (name, description, location, dateTime, user_id) 
+		VALUES (?, ?, ?, ?, ?)
+	`
+	stmt, err := db.DB.Prepare(query) 
+	
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	result , err := stmt.Exec(event.Name, event.Description, event.Location, event.DateTime, event.UserID)
+	
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+	event.ID = id
 	events = append(events, event)
+	return nil
 }
 
-func GetAllEvents() []Event{
-	return events
+func GetAllEvents() ([]Event, error){
+	query := `
+		SELECT * FROM events
+	`
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var events []Event
+	for rows.Next(){
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+	return events, nil
 }
